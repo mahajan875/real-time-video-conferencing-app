f1e7ea00c0085a09eb916e9d0ed3639c6efbd88a
Achyut Mahajan <achyutmahajan@users.noreply.github.com>
2026-01-28 20:00:41 +0530

    Update backend production URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,4 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
+    "https://real-time-video-conferencing-app-3.onrender.com" :
     "http://localhost:8000"
-
-
-export default server;
